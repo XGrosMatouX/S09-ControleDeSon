16ebba281709834bccaaeb91cee58da8830aabb0
XGrosMatouX <louismagnan65@gmail.com>
2025-03-28 12:06:53 -0400

    audio et lumiere ajuster

---
+++ Assets/GestionaireMusique.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionaireMusique : MonoBehaviour
+{
+    public void ChangerVolumeMusique(float nouveauVolume)
+    {
+        
+    }
+}
