dbc03ad1840e442a741165cf2f62165435b54cbc
XGrosMatouX <louismagnan65@gmail.com>
2025-05-09 11:50:08 -0400

    SONS

--- Assets/GestionaireMusique.cs
+++ Assets/GestionaireMusique.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class GestionaireMusique : MonoBehaviour
 {
+    [SerializeField] AudioMixer audioMixer;
     public void ChangerVolumeMusique(float nouveauVolume)
     {
-        
+        audioMixer.SetFloat("VolumeMusique", nouveauVolume);
     }
 }
